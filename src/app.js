fa22de7364f6e4b551cb3d224d2d05e61bfc9afa
Jairo Martinez Tello <150602520+AdminSantrax@users.noreply.github.com>
2026-01-28 20:11:59 +0000

    refactory complete

--- src/app.js
+++ src/app.js
@@ -5,26 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  function getRandomIndex(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  function generaterandomstring() {
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
+    const randomString = getRandomIndex(who) + " " + getRandomIndex(action) + " " + getRandomIndex(what) + " " + getRandomIndex(when);
+    let finalExcuse = document.getElementById("excuse")
 
-const whorandom = Math.floor(Math.random() * who.length);
-const actionrandom = Math.floor(Math.random() * action.length);
-const whatrandom = Math.floor(Math.random() * what.length);
-const whenrandom = Math.floor(Math.random() * when.length);
+    finalExcuse.innerHTML = randomString;
+  }
+  generaterandomstring();
 
-let nam=who[whorandom]+' '+action[actionrandom]+' '+what[whatrandom]+' '+when[whenrandom]
-
-let escusejs = document.getElementById("excuse")
-
-escusejs.innerHTML=nam
-
-
-
+  document
+    .getElementById("randomizerBtn")
+    .addEventListener("click", generaterandomstring);
 };
-  console.log("Hello Rigo from the console!");
+console.log("Hello Rigo from the console!");
